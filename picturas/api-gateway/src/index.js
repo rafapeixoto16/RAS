6c27b96add1a5ea57f72ac36def519eda08dc76c
ruippgoncalves <rgoncalves0320@gmail.com>
2024-12-27 11:08:52 +0000

    CORS and Router

---
+++ picturas/api-gateway/src/index.js
@@ -0,0 +1,32 @@
+import express, { createError } from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import v1Router from './v1/index.js';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Default configs
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(cors()); // TODO we might need to properly configure this
+
+// Routers
+app.use('/v1', v1Router);
+
+// 404
+app.use((req, res, next) => {
+    next(createError(404));
+});
+
+// Error Handler
+app.use((err, req, res) => {
+    // TODO not sure if this is right, I must check
+    res.status(err.status || 500)
+        .send();
+});
+
+// Listen
+app.listen(port, () => {
+    console.log('Server started on port ' + port);
+});
