eef68bee24fab483522b1a888855795c8909aac0
ruippgoncalves <rgoncalves0320@gmail.com>
2024-12-27 11:07:41 +0000

    Auth Middlewares Declaration

---
+++ picturas/api-gateway/src/auth/auth.js
@@ -0,0 +1,31 @@
+import jwt from 'jsonwebtoken';
+
+const secret = process.env.AUTH_JWT_SECRET;
+
+export const checkAuthToken = (req, res, next) => {
+    const token = req.headers['authorization'];
+
+    if (!token) {
+        next();
+        return;
+    }
+
+    jwt.verify(token, secret, (err, decoded) => {
+        if (err) {
+            res.status(401)
+                .send({});
+        } else {
+            req.user = decoded;
+            next();
+        }
+    });
+};
+
+export const requiresAuth = (req, res, next) => {
+    if (!req.user) {
+        res.status(401)
+            .send({});
+    } else {
+        next();
+    }
+};
