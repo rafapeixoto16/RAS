59cb3f248329944b9e748dd24ca48d4059d198c4
rafapeixoto16 <a96807@alunos.uminho.pt>
2024-12-31 16:40:58 +0000

    Rotation filter

---
+++ picturas/rotate-filter/src/index.js
@@ -0,0 +1,16 @@
+import { createFilterHandler, schemaValidation } from '@picturas/filter-helper';
+import sharp from 'sharp';
+
+const rotationSchema = schemaValidation.object({
+    rotationAngle: schemaValidation.number().min(-180).max(180)
+});
+
+async function rotationHandler(imageBuffer, _, params) {
+    const { angle } = params;
+
+    return sharp(imageBuffer)
+        .rotate(angle)
+        .toBuffer();
+}
+
+createFilterHandler('rotation', rotationSchema, rotationHandler);
