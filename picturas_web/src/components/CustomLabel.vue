affcbee4559b362c8ae6be143e68cdfe18b48416
jojocoelho <jhcoelho03@gmail.com>
2025-01-02 00:49:31 +0000

    refator: login and register pages

---
+++ picturas_web/src/components/CustomLabel.vue
@@ -0,0 +1,11 @@
+<template>
+  <label :for="htmlFor" class="block text-sm font-medium text-blue-700 mb-1">
+    <slot></slot>
+  </label>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  htmlFor: string;
+}>();
+</script>
