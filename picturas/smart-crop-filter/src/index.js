05e8fbee7ce02ef5b546235f0f3cbad1c410a151
rafapeixoto16 <a96807@alunos.uminho.pt>
2025-01-03 21:23:28 +0000

    Rotation filter

--- picturas/smart-crop-filter/src/index.js
+++ picturas/smart-crop-filter/src/index.js
@@ -57,7 +57,7 @@ async function smartCropHandler(imageBuffer, _, _params) {
         }
     );
 
-    // Extract the tight bounding box using area
+    // Extract the tight bounding box
     const { xMin, yMin, xMax, yMax } = tightBbox;
     const x = Math.max(Math.floor(xMin), 0);
     const y = Math.max(Math.floor(yMin), 0);
