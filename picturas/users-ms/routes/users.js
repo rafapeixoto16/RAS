332885d753bcce45f64dbe74d2a434e1480bf80f
rafapeixoto16 <rafaelp22um@gmail.com>
2025-01-06 22:00:13 +0000

    Env example.

--- picturas/users-ms/routes/users.js
+++ picturas/users-ms/routes/users.js
@@ -24,6 +24,8 @@ router.post('/login', async (req, res) => {
         const filteredUser = { _id: user._id, }
 
         const accessToken = jwt.sign(filteredUser, process.env.AUTH_JWT_SECRET, { expiresIn: '24h'});
+
+
     }
 
     try {
@@ -104,3 +106,26 @@ router.delete('/logout', (req, res) => {
         userInfo.accessId = null;
         User.updateUser(userInfo._id, userInfo).then(resp => res.sendStatus(200)).catch(err => res.sendStatus(400));});
 });
+
+function sendEmail (){
+
+    const transporter = nodemailer.createTransport({
+        host: process.env.EMAIL_HOSTNAME,
+        port: process.env.EMAIL_PORT,
+        secure: false, // true for port 465, false for other ports
+        auth: {
+            user: `no-reply@${process.env.EMAIL_HOSTNAME}`,
+            pass: process.env.EMAIL_PASSWORD,
+        },
+    });
+
+    transporter.sendMail({
+        from: '"Maddison Foo Koch 👻" <maddison53@ethereal.email>', // sender address
+        to: "bar@example.com, baz@example.com", // list of receivers
+        subject: "Hello ✔", // Subject line
+        text: "Hello world?", // plain text body
+        html: "<b>Hello world?</b>", // html body
+    });
+
+
+}
