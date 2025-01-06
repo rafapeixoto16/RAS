25f311119cbb7f212b91b9b745fccedfc5039c16
rafapeixoto16 <rafaelp22um@gmail.com>
2025-01-06 18:22:22 +0000

    User auth

--- picturas/users-ms/models/userModel.js
+++ picturas/users-ms/models/userModel.js
@@ -1,4 +1,7 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+
+let SALT_WORK_FACTOR = 10
 
 const userSchema = new mongoose.Schema({
     nome: { type: String, required: true },
@@ -9,5 +12,35 @@ const userSchema = new mongoose.Schema({
 }, { versionKey: false });
 
 const User = mongoose.model('user', userSchema);
+
+function bcryptEncripter(user, next) {
+    if (!user.isModified('password'))
+        return next();
+
+    bcrypt.genSalt(SALT_WORK_FACTOR, function (err, salt) {
+        if (err) return next(err);
+
+        bcrypt.hash(user.password, salt, function (err, hash) {
+            if (err) return next(err);
+            user.password = hash;
+            next();
+        });
+    });
+}
+
+userSchema.methods.comparePassword = function (candidatePassword, cb) {
+    bcrypt.compare(candidatePassword, this.password, function (err, isMatch) {
+        if (err) return cb(err);
+        cb(null, isMatch);
+    });
+};
+
+userSchema.pre('save', function (next) {
+    bcryptEncripter(this, next);
+});
+
+userSchema.pre('findOneAndUpdate', function (next) {
+    bcryptEncripter(this, next);
+});
 
 export {User}
