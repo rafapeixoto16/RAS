b61740c1881220d51ebaefdda86d0c647bbb3e24
ruippgoncalves <rgoncalves0320@gmail.com>
2025-01-07 19:16:19 +0000

    Core Filter Logic

--- picturas/filter-helper/src/index.js
+++ picturas/filter-helper/src/index.js
@@ -1,81 +1,130 @@
 import { z } from 'zod';
+import amqp from 'amqplib';
 import { zodToJsonSchema } from 'zod-to-json-schema';
 import { readFileSync, writeFileSync } from 'node:fs';
-import { fileURLToPath } from 'node:url';
-import path from 'node:path';
 
-function exportSchema(filterName, paramsSchema) {
-    const args = process.argv.slice(2);
-
-    if (args.length === 0) process.exit(1);
-
-    const schemaPath = args[0];
-
-    const fileContent = readFileSync(schemaPath, 'utf-8');
-    const jsonData = JSON.parse(fileContent);
-
-    jsonData[filterName] = zodToJsonSchema(paramsSchema, filterName);
-
-    writeFileSync(schemaPath, JSON.stringify(jsonData, null, 2), 'utf-8');
-
-    process.exit(0);
-}
-
-async function downloadImage(imagePath) {
-    return readFileSync(imagePath);
-}
-
-async function uploadImage(buffer, outputPath) {
-    const outputFilePath = path.resolve(outputPath);
-    writeFileSync(outputFilePath, buffer);
-}
+export { z as schemaValidation };
 
 export function createFilterHandler(filterName, paramsSchema, imageHandler) {
     if (process.env.EXPORT_SCHEMA === 'true') {
-        exportSchema(filterName, paramsSchema);
+        const args = process.argv.slice(2);
+
+        if (args.length === 0) process.exit(1);
+
+        const schemaPath = args[0];
+
+        const fileContent = readFileSync(schemaPath, 'utf-8');
+        const jsonData = JSON.parse(fileContent);
+
+        jsonData[filterName] = zodToJsonSchema(paramsSchema, filterName);
+
+        writeFileSync(schemaPath, JSON.stringify(jsonData, null, 2), 'utf-8');
+
+        process.exit(0);
     }
 
-    const params = {};
-    const imagePath = path.join(
-        path.dirname(fileURLToPath(import.meta.url)),
-        '../../sample.jpg'
-    );
-    const outputPath = 'sample-proc';
+    const inputQueue = filterName;
+    const outputQueue = 'filterResponseQueue';
 
-    // TODO use env test for running with sample image
+    async function processMessage(message) {
+        const content = JSON.parse(message.content.toString());
+        const {
+            messageId,
+            parameters,
+        } = content;
+        const {
+            inputImageURI,
+            outputImageURI,
+            ...args
+        } = parameters;
+
+        let processingTime = 0;
+        let data = {};
+        let error = false;
+
+        const validatedParams = paramsSchema.safeParse(args);
+        if (!validatedParams.success) {
+            error = true;
+            data = validatedParams.errors;
+        } else {
+            try {
+
+                const inputFormat = inputImageURI.split('.')
+                    .pop();
+                const imageBuffer = await readFileSync(inputImageURI);
+
+                const start = Date.now();
+                const result = await imageHandler(
+                    imageBuffer,
+                    inputFormat,
+                    validatedParams.data,
+                );
+                const end = Date.now();
+                processingTime = (end - start) / 1000;
+
+                let output,
+                    outputFormat;
+
+                if (Array.isArray(result)) {
+                    [output, outputFormat] = result;
+
+                    if (!['png', 'jpg', 'jpeg', 'bmp', 'webp', 'tiff', 'json'].includes(outputFormat)) {
+                        throw new Error('Invalid output format provided');
+                    }
+                } else {
+                    output = result;
+                    outputFormat = inputFormat;
+                }
+
+                const kind = outputFormat === 'json' ? 'text' : 'image';
+                const outputPath = inputImageURI.split('.')
+                    .first();
+                const uploadedImageURI = `${outputPath}.${outputFormat}`;
+
+                await writeFileSync(uploadedImageURI, output);
+
+                data = {
+                    type: kind,
+                    imageURI: uploadedImageURI,
+                };
+            } catch (err) {
+                error = true;
+                data = err;
+            }
+        }
+
+        return {
+            messageId: `completion-${messageId}`,
+            correlationId: messageId,
+            timestamp: Date.now()
+                .toString(),
+            status: error ? 'error' : 'success',
+            [error ? 'error' : 'output']: data,
+            metadata: {
+                processingTime: 0,
+                microservice: filterName,
+            },
+        };
+    }
 
     (async () => {
-        const validatedParams = paramsSchema.safeParse(params);
+        const connection = await amqp.connect('amqp://localhost');
+        const channel = await connection.createChannel();
 
-        if (!validatedParams.success) {
-            // validatedParams.error
-            process.exit(1);
-        }
+        await channel.assertQueue(inputQueue, { durable: true });
+        await channel.assertQueue(outputQueue, { durable: true });
 
-        const inputFormat = imagePath.split('.').pop();
-        const imageBuffer = await downloadImage(imagePath);
-        const result = await imageHandler(
-            imageBuffer,
-            inputFormat,
-            validatedParams.data
-        );
+        channel.consume(inputQueue, async (message) => {
+            if (message) {
+                const content = JSON.parse(message.content.toString());
+                const result = await processMessage(content);
 
-        let output;
-        let outputFormat;
+                channel.sendToQueue(outputQueue, Buffer.from(JSON.stringify(result)), {
+                    persistent: true,
+                });
 
-        if (Array.isArray(result)) {
-            [output, outputFormat] = result;
-        } else {
-            output = result;
-            outputFormat = inputFormat;
-        }
-
-        // TODO content hash for name?
-
-        await uploadImage(output, `${outputPath}.${outputFormat}`);
+                channel.ack(message);
+            }
+        });
     })();
-
-    setTimeout(() => {}, 1000);
 }
-
-export { z as schemaValidation };
