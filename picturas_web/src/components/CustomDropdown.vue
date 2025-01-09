ab016731d8c2412c41cb3e2c003ea08dacd47d26
filipefelix10 <filipefelix10@hotmail.com>
2025-01-09 00:26:57 +0000

    router-link started

--- picturas_web/src/components/CustomDropdown.vue
+++ picturas_web/src/components/CustomDropdown.vue
@@ -6,7 +6,7 @@
         </span>
         <transition name="dropdown">
             <Teleport v-if="appendToBody && isOpen" to="body">
-                <div :style="dropdownStyle" class="absolute shadow-md rounded mt-2 border border-gray-200 z-50 text-gray-700" ref="dropdownElement">
+                <div :style="dropdownStyle" class="absolute bg-blue-50 shadow-md rounded mt-2 border border-gray-200 z-50 text-gray-700" ref="dropdownElement">
                     <ul>
                         <li 
                             v-for="option in options" 
@@ -25,14 +25,14 @@
                     </ul>
                 </div>
             </Teleport>
-            <div v-else-if="isOpen" class="absolute shadow-md rounded mt-2 border border-gray-200 z-50" ref="dropdownElement">
+            <div v-else-if="isOpen" class="absolute bg-[#F5F7FA] shadow-md rounded mt-2 border border-gray-200 z-50" ref="dropdownElement">
                 <ul>
                     <li 
                         v-for="option in options" 
                         :key="option.label" 
                         class="flex items-center gap-2 px-4 py-2 hover:bg-gray-100 cursor-pointer"
                     >
-                        <router-link v-if="option.route" :to="option.route" class="flex items-center gap-2">
+                        <router-link v-if="option.route" :to="!option.target ? option.route : undefined" class="flex items-center gap-2"  @click.prevent="openInNewTab(option.route)">
                             <i v-if="option.icon" :class="option.icon"></i>
                             <span class="text-gray-700">{{ option.label }}</span>
                         </router-link>
@@ -48,21 +48,21 @@
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted, onBeforeUnmount } from 'vue';
-import { RouterLink } from 'vue-router';
+import { ref, onMounted, onBeforeUnmount } from "vue";
+import { RouterLink } from "vue-router";
 
 interface Option {
-    label: string;
-    icon?: string;
-    route?: string;
+  label: string;
+  icon?: string;
+  route?: string;
 }
 
 interface Props {
-    placement: 'right' | 'left' | 'top' | 'bottom';
-    trigger: string;
-    options: Option[];
-    isIcon?: boolean;
-    appendToBody?: boolean;
+  placement: "right" | "left" | "top" | "bottom";
+  trigger: string;
+  options: Option[];
+  isIcon?: boolean;
+  appendToBody?: boolean;
     menuColor?: string;  // Add a new prop for menu color
 }
 
@@ -81,79 +81,92 @@ const dropdownElement = ref<HTMLElement | null>(null);
 const dropdownStyle = ref({});
 
 const toggleDropdown = () => {
-    isOpen.value = !isOpen.value;
-    if (isOpen.value && appendToBody) {
-        updateDropdownPosition();
-    }
+  isOpen.value = !isOpen.value;
+  if (isOpen.value && appendToBody) {
+    updateDropdownPosition();
+  }
 };
 
 const updateDropdownPosition = () => {
-    if (triggerElement.value) {
-        const rect = triggerElement.value.getBoundingClientRect();
-        dropdownStyle.value = {
-            top: `${rect.bottom + window.scrollY}px`,
-            left: `${rect.left + window.scrollX}px`,
+  if (triggerElement.value) {
+    const rect = triggerElement.value.getBoundingClientRect();
+    dropdownStyle.value = {
+      top: `${rect.bottom + window.scrollY}px`,
+      left: `${rect.left + window.scrollX}px`,
             backgroundColor: menuColor, // Apply the background color from the prop
-        };
-    }
+    };
+  }
+};
+
+const openInNewTab = (route) => {
+  console.log(route);
+  // Open the URL in a new tab
+  window.open(route, "_blank");
 };
 
 const handleClickOutside = (event: MouseEvent | TouchEvent) => {
-    if (dropdownElement.value && !dropdownElement.value.contains(event.target as Node) && !triggerElement.value?.contains(event.target as Node)) {
-        isOpen.value = false;
-    }
+  if (
+    dropdownElement.value &&
+    !dropdownElement.value.contains(event.target as Node) &&
+    !triggerElement.value?.contains(event.target as Node)
+  ) {
+    isOpen.value = false;
+  }
 };
 
 onMounted(() => {
-    window.addEventListener('resize', updateDropdownPosition);
-    window.addEventListener('scroll', updateDropdownPosition);
-    document.addEventListener('mousedown', handleClickOutside);
-    document.addEventListener('touchstart', handleClickOutside);
+  window.addEventListener("resize", updateDropdownPosition);
+  window.addEventListener("scroll", updateDropdownPosition);
+  document.addEventListener("mousedown", handleClickOutside);
+  document.addEventListener("touchstart", handleClickOutside);
 });
 
 onBeforeUnmount(() => {
-    window.removeEventListener('resize', updateDropdownPosition);
-    window.removeEventListener('scroll', updateDropdownPosition);
-    document.removeEventListener('mousedown', handleClickOutside);
-    document.removeEventListener('touchstart', handleClickOutside);
+  window.removeEventListener("resize", updateDropdownPosition);
+  window.removeEventListener("scroll", updateDropdownPosition);
+  document.removeEventListener("mousedown", handleClickOutside);
+  document.removeEventListener("touchstart", handleClickOutside);
 });
 </script>
 
 <style scoped>
 .relative.bottom .absolute {
-    top: calc(100% + 8px);
-    left: 0;
+  top: calc(100% + 8px);
+  left: 0;
 }
 
 .relative.top .absolute {
-    bottom: calc(100% + 8px);
-    left: 0;
+  bottom: calc(100% + 8px);
+  left: 0;
 }
 
 .relative.left .absolute {
-    top: 0;
-    right: calc(100% + 8px);
+  top: 0;
+  right: calc(100% + 8px);
 }
 
 .relative.right .absolute {
-    top: 0;
-    left: calc(100% + 8px);
+  top: 0;
+  left: calc(100% + 8px);
 }
 
-.dropdown-enter-active, .dropdown-leave-active {
-    transition: opacity 0.3s, transform 0.3s;
+.dropdown-enter-active,
+.dropdown-leave-active {
+  transition: opacity 0.3s, transform 0.3s;
 }
 
-.dropdown-enter-from, .dropdown-leave-to {
-    opacity: 0;
-    transform: translateY(-10px);
+.dropdown-enter-from,
+.dropdown-leave-to {
+  opacity: 0;
+  transform: translateY(-10px);
 }
 
 .absolute {
-    width: max-content;
+  width: max-content;
 }
 
 .absolute ul {
-    display: inline-block;
+  display: inline-block;
 }
 </style>
+
--- picturas_web/src/components/SideBar.vue
+++ picturas_web/src/components/SideBar.vue
@@ -1,18 +1,28 @@
 <template>
   <div class="flex h-screen">
-      <div class="fixed left-0 flex flex-col w-1/6 h-full bg-blue-50 text-gray-700 border-r border-gray-200 shadow-lg">
-          <router-link class="flex items-center justify-center px-8 py-4 w-full" to="/">
-              <h1 class="text-3xl xl:text-5xl font-bold text-gray-900" style="font-family: 'Caveat', cursive;">Picturas</h1>
-          </router-link>
-          <div class="px-4 space-y-4">
-              <router-link
-                  class="flex items-center justify-center px-2 py-3 w-full bg-[#3B82F6] text-sm xl:text-base text-white hover:bg-[#2563EB] rounded-xl"
-                  to="/create-project"
-              >
-                  <i class="bi bi-plus mr-2 fs-5"></i>
-                  Create a Project
-              </router-link>
-              <a
+    <div
+      class="fixed left-0 flex flex-col w-1/6 h-full bg-blue-50 text-gray-700 border-r border-gray-200 shadow-lg"
+    >
+      <router-link
+        class="flex items-center justify-center px-8 py-4 w-full"
+        to="/"
+      >
+        <h1
+          class="text-3xl xl:text-5xl font-bold text-gray-900"
+          style="font-family: 'Caveat', cursive;"
+        >
+          Picturas
+        </h1>
+      </router-link>
+      <div class="px-4 space-y-4">
+        <router-link
+          class="flex items-center justify-center px-2 py-3 w-full bg-[#3B82F6] text-sm xl:text-base text-white hover:bg-[#2563EB] rounded-xl"
+          to="/create-project"
+        >
+          <i class="bi bi-plus mr-2 fs-5"></i>
+          Create a Project
+        </router-link>
+        <a
                 href="#"
                 class="flex items-center justify-center px-2 py-3 w-full bg-[#ff9800] text-sm xl:text-base text-white hover:bg-[#e68900] rounded rounded-xl"
                 @click="openPremiumModal"
@@ -20,40 +30,49 @@
                 <i class="bi bi-gem mr-2"></i>
                 Try Pro for 30 days
               </a>
+            </div>
+      <div
+        class="flex flex-col items-start mt-8 px-4 space-y-2 flex-grow overflow-y-auto"
+      >
+        <h2 class="text-sm font-semibold">Projects</h2>
+        <div class="flex flex-col w-full relative">
+          <div
+            v-for="project in visibleProjects"
+            :key="project.name"
+            class="relative group w-full"
+          >
+            <router-link class="block p-4 rounded w-full" :to="project.link">
+              {{ project.name }}
+            </router-link>
+            <div
+              class="absolute right-0 top-1/2 transform -translate-y-1/2 opacity-0 group-hover:opacity-100 transition-opacity z-10"
+            >
+              <Dropdown
+                placement="right"
+                trigger="..."
+                :options="[
+                  {
+                    label: 'Open in a new tab',
+                    icon: 'bi bi-box-arrow-up-right',
+                    route: project.link,
+                    target: true,
+                  },{label: 'Rename', icon: 'bi bi-pencil'},
+                  { label: 'Move to Trash', icon: 'bi bi-trash' },
+                ]"
+                append-to-body
+              />
+            </div>
           </div>
-          <div class="flex flex-col items-start mt-8 px-4 space-y-2 flex-grow overflow-y-auto">
-              <h2 class="text-sm font-semibold">Projects</h2>
-              <div class="flex flex-col w-full relative">
-                  <div
-                      v-for="project in visibleProjects"
-                      :key="project.name"
-                      class="relative group w-full"
-                  >
-                      <router-link class="block p-4 rounded w-full" :to="project.link">
-                          {{ project.name }}
-                      </router-link>
-                      <div
-                          class="absolute right-0 top-1/2 transform -translate-y-1/2 opacity-0 group-hover:opacity-100 transition-opacity z-10"
-                      >
-                          <Dropdown
-                              placement="right"
-                              trigger="..."
-                              :options="[{ label: 'Open in a new tab', icon: 'bi bi-box-arrow-up-right' },{label: 'Rename', icon: 'bi bi-pencil'}, { label: 'Move to Trash', icon: 'bi bi-trash' }]"
-                              :menu-color="'#EFF6FF'"
-                              append-to-body
-                          />
-                      </div>
-                  </div>
-                  <button
-                      v-if="showSeeAllButton"
-                      @click="toggleSeeAll"
-                      class="mt-2 text-[#3B82F6] hover:underline"
-                  >
-                      {{ seeAll ? 'See Less' : 'See All' }}
-                  </button>
-              </div>
-          </div>
-          <div class="px-4 py-2 border-t border-gray-200">
+          <button
+            v-if="showSeeAllButton"
+            @click="toggleSeeAll"
+            class="mt-2 text-[#3B82F6] hover:underline"
+          >
+            {{ seeAll ? "See Less" : "See All" }}
+          </button>
+        </div>
+      </div>
+      <div class="px-4 py-2 border-t border-gray-200">
               <router-link
                   class="flex items-center px-4 py-3 w-full bg-red-500 text-sm xl:text-base text-white hover:bg-red-600 rounded-xl"
                   to="/trash"
@@ -62,40 +81,40 @@
                   Trash
               </router-link>
           </div>
-          <div class="mt-auto w-full">
-              <ProfileMenu />
-          </div>
-          <premium-upgrade
+      <div class="mt-auto w-full">
+        <ProfileMenu />
+      </div>
+      <premium-upgrade
             :open="isOpenPremium"
             @close="openPremiumModal"
           ></premium-upgrade>
-      </div>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref, computed } from 'vue';
-import Dropdown from './CustomDropdown.vue'
-import ProfileMenu from './ProfileMenu.vue';
+import { ref, computed } from "vue";
+import Dropdown from "./CustomDropdown.vue";
+import ProfileMenu from "./ProfileMenu.vue";
 import PremiumUpgrade from './PremiumUpgrade.vue';
 
 const projects = [
-  { name: 'Project 1', link: '/project1' },
-  { name: 'Project 2', link: '/project2' },
-  { name: 'Project 3', link: '/project3' },
-  { name: 'Project 4', link: '/project4' },
-  { name: 'Project 5', link: '/project5' },
-  { name: 'Project 6', link: '/project6' },
-  { name: 'Project 7', link: '/project7' },
-  { name: 'Project 8', link: '/project8' },
-  { name: 'Project 1', link: '/project1' },
-  { name: 'Project 2', link: '/project2' },
-  { name: 'Project 3', link: '/project3' },
-  { name: 'Project 4', link: '/project4' },
-  { name: 'Project 5', link: '/project5' },
-  { name: 'Project 6', link: '/project6' },
-  { name: 'Project 7', link: '/project7' },
-  { name: 'Project 8', link: '/project8' },
+  { name: "Project 1", link: "/project1" },
+  { name: "Project 2", link: "/project2" },
+  { name: "Project 3", link: "/project3" },
+  { name: "Project 4", link: "/project4" },
+  { name: "Project 5", link: "/project5" },
+  { name: "Project 6", link: "/project6" },
+  { name: "Project 7", link: "/project7" },
+  { name: "Project 8", link: "/project8" },
+  { name: "Project 1", link: "/project1" },
+  { name: "Project 2", link: "/project2" },
+  { name: "Project 3", link: "/project3" },
+  { name: "Project 4", link: "/project4" },
+  { name: "Project 5", link: "/project5" },
+  { name: "Project 6", link: "/project6" },
+  { name: "Project 7", link: "/project7" },
+  { name: "Project 8", link: "/project8" },
 ];
 
 const seeAll = ref(false);
@@ -103,13 +122,16 @@ const isOpenPremium = ref(false);
 
 const visibleProjects = computed(() => {
   return seeAll.value ? projects : projects.slice(0, 7);
+  return seeAll.value ? projects : projects.slice(0, 7);
 });
 
 const showSeeAllButton = computed(() => {
   return projects.length > 7;
+  return projects.length > 7;
 });
 
 const toggleSeeAll = () => {
+  seeAll.value = !seeAll.value;
   seeAll.value = !seeAll.value;
 };
 
@@ -125,24 +147,25 @@ const openPremiumModal = () => {
 }
 .overflow-y-auto {
   overflow-y: auto;
+  overflow-y: auto;
 }
 
 .overflow-y-auto::-webkit-scrollbar-track {
-  background: #F5F7FA;
+  background: #f5f7fa;
 }
 
 .overflow-y-auto::-webkit-scrollbar-thumb {
-  background-color: #3B82F6;
+  background-color: #3b82f6;
   border-radius: 10px;
-  border: 2px solid #F5F7FA;
+  border: 2px solid #f5f7fa;
 }
 
 .overflow-y-auto::-webkit-scrollbar-thumb:hover {
-  background-color: #2563EB;
+  background-color: #2563eb;
 }
 
 .overflow-y-auto {
   scrollbar-width: thin;
-  scrollbar-color: #3B82F6 #F5F7FA;
+  scrollbar-color: #3b82f6 #f5f7fa;
 }
 </style>
