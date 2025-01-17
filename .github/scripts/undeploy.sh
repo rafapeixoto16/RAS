a5bc969799e6d99179f0d6e61946799c4bff4b88
ruippgoncalves <rgoncalves0320@gmail.com>
2025-01-17 18:34:11 +0000

    Deploy

--- .github/scripts/undeploy.sh
+++ .github/scripts/undeploy.sh
@@ -15,11 +15,18 @@ curl -fsSL https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 |
 ssh -o StrictHostKeyChecking=no -f -N -L 6443:127.0.0.1:6443 $SSH_USER@$SSH_HOST -p $SSH_PORT -i private_key &
 sleep 5
 
+# Configure docker
+docker context create server-remote --docker "host=ssh://$SSH_USER@$SSH_HOST:$SSH_PORT"
+docker context use server-remote
+
 # Config Kubernetes
 mkdir $HOME/.kube
 ssh -o StrictHostKeyChecking=no $SSH_USER@$SSH_HOST -p $SSH_PORT -i private_key 'cat $HOME/.kube/config' > $HOME/.kube/config
 sed -i 's/127.0.0.1:[0-9]\+/localhost:6443/g' $HOME/.kube/config
 export KUBECONFIG=$HOME/.kube/config
+
+# Build Images
+docker system prune --all -y
 
 # Helm uninstall
 helm uninstall picturas
