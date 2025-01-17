f202d286a4cdd6b70ff1327a19279136f502da9b
ruippgoncalves <rgoncalves0320@gmail.com>
2025-01-17 10:33:31 +0000

    Debug Messages

--- picturas/subscriptions-ms/index.js
+++ picturas/subscriptions-ms/index.js
@@ -41,7 +41,6 @@ initStripe().then(() => {
 app.use(morgan('dev'));
 app.use(express.json({
     verify: (req, res, buf, _) => {
-        console.error(buf.length, req.originalUrl)
         if (req.originalUrl.startsWith('/webhook')) {
             req.rawBody = buf; // Save raw body for Stripe webhook verification
         }
